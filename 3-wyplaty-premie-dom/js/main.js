e1850422f376b49efa06bbc22effeeecfcaaea47
Michał Noszczyk <noszczykmichal@wp.pl>
2020-01-23 22:24:32 +0100

    'uzupełnienie funkcji'

--- 3-wyplaty-premie-dom/js/main.js
+++ 3-wyplaty-premie-dom/js/main.js
@@ -2,13 +2,20 @@
 let inputCzas= document.getElementsByClassName('czas');
 let inputStawka=document.getElementsByClassName('stawka');
 let spanWyplata=document.getElementsByClassName('wyplata');
+let spanPracownik=document.getElementsByClassName('pracownik');
+// tablica ze wszystkimi wartościami przepracowanych godzin
+let arrayCzas=[];
+//tablica z trzem największymi wartościami
+let trzyNajwieksze=[];
 
 //weryfikacja poprawności pobrania elementów
-console.log(inputCzas);
+// console.log(inputCzas);
 
 function wynagrodzenie(){
 
-for(let i =0; i<inputCzas.length; i++){
+    // pierwsza pętla
+
+for(let i=0; i<inputCzas.length; i++){
     //weryfikacja działania pętli
     // console.log(`Index elementu: ${i} + wartość elementu + ${inputCzas[i].value}`);   
     if(inputCzas[i].value>160){
@@ -21,9 +28,51 @@ for(let i =0; i<inputCzas.length; i++){
         let wysokoscWyplaty= inputCzas[i].value*inputStawka[i].value
         spanWyplata[i].innerText=wysokoscWyplaty;
     }
-       
+   
+    // //weryfikacja pracowników którzy przepracowali poniżej 100 godzin
+    if(inputCzas[i].value<100){
+        spanPracownik[i].setAttribute('class', 'red');
     }
+    };
+
+    //koniec pierwszej pętli
+    for(let i=0; i<inputCzas.length; i++){
+        let godziny=inputCzas[i].value;
+        arrayCzas.push(godziny);
+
+    }
+
+    function sortNumber(a,b){
+        return b-a;
+    }
+    arrayCzas.sort(sortNumber);
+
+    console.log('Tablica arrayCzas: ', arrayCzas);
+
+    //utworzenie tablicy z trojgiem najwięszych liczb przepracowanych godzin
+    for(let i=0; i<3; i++){
+        let czas=parseInt(arrayCzas[i]);
+        trzyNajwieksze.push(czas);
+    }
+
+    // console.log('Tablica trzyNajwieksze: ',trzyNajwieksze);
+
+    //pętla iteruje po wszystkich inputach classy czas i sprawdza czy ich wartość jest jedną z tablicy TrzyNajwieksze jeśli tak to w console.logu wyświetla się zawartość spanuPracownik o tym samym indexie-wszystkich elemntów o danej klasie jest tyle samo
+
+    for(let i=0; i<inputCzas.length; i++){
+        let wartoscInputCzas=inputCzas[i].value;
+        for(let j=0; j<trzyNajwieksze.length;j++){
+          let argumentTrzyNajwieksze=trzyNajwieksze[j];
+          if(wartoscInputCzas == argumentTrzyNajwieksze){
+            console.log(spanPracownik[i].innerText)
+          }
+        }
+      };
     
+     
+
 }
 
+
 wynagrodzenie();
+
