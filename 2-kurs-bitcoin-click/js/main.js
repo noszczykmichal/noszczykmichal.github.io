32f6eee080dd5aacb7f11189290665f3446bb835
Michał Noszczyk <noszczykmichal@wp.pl>
2020-02-12 22:13:31 +0100

    Dodanie pobierania kursu sprzedazy + skrocenie kodu

--- 2-kurs-bitcoin-click/js/main.js
+++ 2-kurs-bitcoin-click/js/main.js
@@ -1,13 +1,14 @@
 $(function () {
-  let kursPoczatkowyKupno= null;
+  let kursPoczatkowyKupno=null;
+  let kursPoczatkowySprzedaz=null;
   $.getJSON('https://blockchain.info/pl/ticker', function (result) {
     // console.log(result)
 
-    kursPoczatkowyKupno = result['PLN'].buy;
+    kursPoczatkowyKupno=result['PLN'].buy;
     console.log("TCL: kursPoczatkowyKupno", kursPoczatkowyKupno)
     
-
-    
+    kursPoczatkowySprzedaz=result['PLN'].sell;
+    console.log("TCL: kursPoczatkowySprzedaz", kursPoczatkowySprzedaz)
 
   })
 
@@ -15,28 +16,42 @@ $(function () {
     $.getJSON('https://blockchain.info/pl/ticker', function (result) {
     kursAktualnyKupno=result['PLN'].buy
     console.log("TCL: sprawdzKurs -> kursAktualnyKupno", kursAktualnyKupno)
+    // wstrzykuje aktualną wartosc kursu->.kupnoWartosc
+    $('.kupnoWartosc').text(kursAktualnyKupno)
 
     if(kursAktualnyKupno>kursPoczatkowyKupno){
       // console.log('kurs wzrósł')
-      //w zależności od wyniku działania if'a; wstrzykuje aktualną wartosc kursu->.kupnoWartosc; a do do spanu .kupno odpowiednią ikonę
-      $('.kupnoWartosc').text(kursAktualnyKupno)
+      //w zależności od wyniku działania if'a do spanu .kupno wstawiam odpowiednią ikonę
       $('.kupno').html('<i class="fas fa-arrow-up"></i>')
       $('.kupno>i').css({'color': 'green'})
     }  else if(kursAktualnyKupno<kursPoczatkowyKupno){
       // console.log('kurs spadł')
-      $('.kupnoWartosc').text(kursAktualnyKupno)
       $('.kupno').html('<i class="fas fa-arrow-down"></i>')
       $('.kupno>i').css({'color': 'red'})
     }else{
       // console.log('kurs nie zmienił się')
-      $('.kupnoWartosc').text(kursAktualnyKupno)
       $('.kupno').html('<i class="fas fa-arrows-alt-h"></i>')
     }
 
-    
-    
-    kursPoczatkowyKupno=kursAktualnyKupno
-    
+    kursAktualnySprzedaz=result['PLN'].sell
+    console.log("TCL: sprawdzKurs -> kursAktualnySprzedaz", kursAktualnySprzedaz)
+    $('.sprzedazWartosc').text(kursAktualnySprzedaz)
+
+    if(kursAktualnySprzedaz>kursPoczatkowySprzedaz){
+      //jak wyżej tylko w odniesieniu do kursu sprzedaży
+      $('.sprzedaz').html('<i class="fas fa-arrow-up"></i>')
+      $('.sprzedaz>i').css({'color': 'green'})
+    } else if(kursAktualnySprzedaz<kursPoczatkowySprzedaz){
+      $('.sprzedaz').html('<i class="fas fa-arrow-down"></i>')
+      $('.sprzedaz>i').css({'color': 'red'})
+    } else {
+      $('.sprzedaz').html('<i class="fas fa-arrows-alt-h"></i>')
+    }
+
+    kursPoczatkowyKupno=kursAktualnyKupno;
+    console.log("TCL: sprawdzKurs -> kursPoczatkowyKupno", kursPoczatkowyKupno)
+    kursPoczatkowySprzedaz=kursAktualnySprzedaz;
+    console.log("TCL: sprawdzKurs -> kursPoczatkowySprzedaz", kursPoczatkowySprzedaz)
     
     })
 
