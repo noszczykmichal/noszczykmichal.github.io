bcc70b008cdf487e23627150559cf063cb4cbc58
Michał Noszczyk <noszczykmichal@wp.pl>
2020-02-12 21:35:31 +0100

    Drobne poprawki w zmiennych

--- 2-kurs-bitcoin-click/js/main.js
+++ 2-kurs-bitcoin-click/js/main.js
@@ -1,37 +1,42 @@
 $(function () {
-  let kursPoczatkowy = null;
+  let kursPoczatkowyKupno= null;
   $.getJSON('https://blockchain.info/pl/ticker', function (result) {
     // console.log(result)
 
-    kursPoczatkowy = result['PLN'].buy;
-    console.log("TCL: kursPoczatkowy", kursPoczatkowy)
+    kursPoczatkowyKupno = result['PLN'].buy;
+    console.log("TCL: kursPoczatkowyKupno", kursPoczatkowyKupno)
+    
+
+    
 
   })
 
   function sprawdzKurs(){
     $.getJSON('https://blockchain.info/pl/ticker', function (result) {
-    kursAktualny=result['PLN'].buy
-    console.log("TCL: sprawdzKurs -> kursAktualny", kursAktualny)
+    kursAktualnyKupno=result['PLN'].buy
+    console.log("TCL: sprawdzKurs -> kursAktualnyKupno", kursAktualnyKupno)
 
-    if(kursAktualny>kursPoczatkowy){
+    if(kursAktualnyKupno>kursPoczatkowyKupno){
       // console.log('kurs wzrósł')
       //w zależności od wyniku działania if'a; wstrzykuje aktualną wartosc kursu->.kupnoWartosc; a do do spanu .kupno odpowiednią ikonę
-      $('.kupnoWartosc').text(kursAktualny)
+      $('.kupnoWartosc').text(kursAktualnyKupno)
       $('.kupno').html('<i class="fas fa-arrow-up"></i>')
       $('.kupno>i').css({'color': 'green'})
-    }  else if(kursAktualny<kursPoczatkowy){
+    }  else if(kursAktualnyKupno<kursPoczatkowyKupno){
       // console.log('kurs spadł')
-      $('.kupnoWartosc').text(kursAktualny)
+      $('.kupnoWartosc').text(kursAktualnyKupno)
       $('.kupno').html('<i class="fas fa-arrow-down"></i>')
       $('.kupno>i').css({'color': 'red'})
     }else{
       // console.log('kurs nie zmienił się')
-      $('.kupnoWartosc').text(kursAktualny)
+      $('.kupnoWartosc').text(kursAktualnyKupno)
       $('.kupno').html('<i class="fas fa-arrows-alt-h"></i>')
     }
+
     
-    kursPoczatkowy=kursAktualny
-    console.log("TCL: sprawdzKurs -> kursPoczatkowy", kursPoczatkowy)
+    
+    kursPoczatkowyKupno=kursAktualnyKupno
+    
     
     })
 
