e7ceb0e3d11cc026b904b4af4dcd6cdc21a7d07a
Michał Noszczyk <noszczykmichal@wp.pl>
2020-02-12 21:26:24 +0100

    Gotowe pobieranie kursu kupna

--- 2-kurs-bitcoin-click/js/main.js
+++ 2-kurs-bitcoin-click/js/main.js
@@ -13,11 +13,28 @@ $(function () {
     kursAktualny=result['PLN'].buy
     console.log("TCL: sprawdzKurs -> kursAktualny", kursAktualny)
 
+    if(kursAktualny>kursPoczatkowy){
+      // console.log('kurs wzrósł')
+      //w zależności od wyniku działania if'a; wstrzykuje aktualną wartosc kursu->.kupnoWartosc; a do do spanu .kupno odpowiednią ikonę
+      $('.kupnoWartosc').text(kursAktualny)
+      $('.kupno').html('<i class="fas fa-arrow-up"></i>')
+      $('.kupno>i').css({'color': 'green'})
+    }  else if(kursAktualny<kursPoczatkowy){
+      // console.log('kurs spadł')
+      $('.kupnoWartosc').text(kursAktualny)
+      $('.kupno').html('<i class="fas fa-arrow-down"></i>')
+      $('.kupno>i').css({'color': 'red'})
+    }else{
+      // console.log('kurs nie zmienił się')
+      $('.kupnoWartosc').text(kursAktualny)
+      $('.kupno').html('<i class="fas fa-arrows-alt-h"></i>')
+    }
     
-
+    kursPoczatkowy=kursAktualny
+    console.log("TCL: sprawdzKurs -> kursPoczatkowy", kursPoczatkowy)
+    
     })
 
-    console.log('działa')
 
   }
 
