ee177eaa2576718ecf16c9eda03ded94ea5a54b2
Michał Noszczyk <noszczykmichal@wp.pl>
2022-03-24 03:24:16 +0100

    code refactoring, fixing errors, all comments in english

--- to-do-list/js/main.js
+++ to-do-list/js/main.js
@@ -1,170 +1,137 @@
-//pobieram elementy do zmiennych
-const clear=document.querySelector(".clear");
-const dateElem=document.getElementById("date");
-const list=document.getElementById("list");
-const input=document.getElementById("input");
-const btnPlus=document.querySelector("[class*=fa-plus-circle]");
-const time=document.getElementById("time");
-// console.log("TCL: btnPlus", btnPlus)
+//assigning elements from DOM to variables
+const clear = document.querySelector(".clear");
+const dateElem = document.getElementById("date");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
+const btnPlus = document.querySelector("[class*=fa-plus-circle]");
+const time = document.getElementById("time");
 
-// zmienne class'y
+// utility classes
+const check = "fa-check-circle";
+const uncheck = "fa-circle";
+const lineThrough = "lineThrough";
 
-const check="fa-check-circle";
-const uncheck="fa-circle";
-const lineThrough="lineThrough";
+let objList;
 
-//zmienne
-let objList, id;
+//assigning variable to data retrieved from localStorage
+let data = localStorage.getItem("TODO");
 
-//pobieranie elementów ze schowka
-let data=localStorage.getItem("TODO");
-
-//sprawdzanie czy w schowków przetrzymywane są dane
-
-if(data){
-    objList=JSON.parse(data);
-    id=objList.length; //określamy jakie id będzie mieć kolejny dodany element sprawdzając długość tablicy
-    LoadObjList(objList); //załadowanie listy po odświeżeniu strony na podstawie danych pobranych ze schowka
-}else{ //jeśli schowek jest pusty- ustawiamy zmienną objList jako tablicę, a wartość id na równą 0
-    objList=[];
-    id=0;
+function loadObjList() {
+  //checking if there are any data that are stored locally
+  if (data) {
+    objList = JSON.parse(data);
+    objList.forEach(item =>
+      addToDo(item.name, item.id, item.done, item.trash)
+    );
+  } else {
+    objList = []; //if there is none an empty array is assigned
+  }
 }
 
-//dodanie funkcji LoadObjList, która załaduje listę elementów przechowywanych w schowku
-function LoadObjList(array){
-    array.forEach(function(item){
-        addToDo(item.name, item.id, item.done, item.trash)
-    });
+loadObjList();//initial list load
+
+//clearing of localStorage
+clear.addEventListener("click", function () {
+  localStorage.clear();
+  location.reload();
+});
+
+//display of current date
+const options = { weekday: "short", month: "long", day: "numeric" };
+const today = new Date();
+
+dateElem.innerHTML = today.toLocaleDateString("pl-PL", options);
+
+//and time
+function whatTime() {
+  const hour = new Date().toLocaleTimeString("pl-PL");
+  return (time.innerHTML = hour);
 }
 
-//usuwanie danych ze schowka
+setInterval(whatTime, 1000);
 
-clear.addEventListener("click", function(){
-    localStorage.clear();
-    location.reload();
-})
+function addToDo(toDo, id, done, trash) {
+  //if element is in trash it won't be rendered to the DOM
+  if (trash) {
+    return;
+  }
 
-//wyświetlanie aktualnej daty
-const options= {weekday:"short", month:"long", day:"numeric"}
-const today= new Date();
+  const isDone = done ? check : uncheck;
+  const line = done ? lineThrough : "";
 
-dateElem.innerHTML=today.toLocaleDateString("pl-PL", options)
-
-//i godziny
-function whatTime(){
-    const hour= (new Date()).toLocaleTimeString("pl-PL");
-    return time.innerHTML=hour;
-}
-
-setInterval(whatTime, 1000)
-
-//funkcja addToDo
-
-function addToDo (toDo, id, done, trash){
-    //jeśli element znajduje się w koszu pozostała część kodu się nie wykona
-    if(trash){return;}
-
-    const isDone=done? check : uncheck;
-    const line=done? lineThrough : ""; 
-
-    const item=`
-                <li class="item">
-                    <i class="far ${isDone} co" job="complete" id="${id}"></i>
+  const item = `
+                <li class="item" id="${id}">
+                    <i class="far ${isDone} co" job="complete"></i>
                     <p class="text ${line}">${toDo}</p>
-                    <i class="far fa-trash-alt de" job="delete" id="${id}"></i>
+                    <i class="far fa-trash-alt de" job="delete"></i>
                 </li>
                 `;
-    const postion="beforeend";
 
-    list.insertAdjacentHTML(postion, item);
+  list.insertAdjacentHTML("beforeend", item);
 }
 
-//dodawanie elementu do listy po wciśnięciu 'Enter'
+//entered input validation -if entered item is valid (is not an empty string) it's added locally to the objList and then to the localStorage
+function validate() {
+  const toDo = input.value.trim(); // using 'trim' to make sure that user didn't enter just whitespaces
+  //and whether now current value of 'toDo' is not equal to an empty string
+  if (toDo !== "") {
+    const id = Math.random().toString();
+    addToDo(toDo, id, false, false);
+    objList.push({
+      name: toDo,
+      id: id,
+      done: false,
+      trash: false,
+    });
 
-document.addEventListener("keyup", function(event){
-    if(event.keyCode==13){
-        const toDo=input.value.trim(); // dodaje trim() dla pewności że nie zostaną przekazane same spacje
-        //sprawdzamy czy z inputa nie jest przekazywana pusty string
-        if(toDo!==""){
-            addToDo(toDo, id, false, false);
-            objList.push({
-                name: toDo,
-                id: id,
-                done: false,
-                trash: false,
-
-            });
-
-            //dodawanie elementów do schowka (kod dodawany w każdym miejscu gdzie dopisywane są dane do tablicy z elementami listy)
-            localStorage.setItem("TODO", JSON.stringify(objList));
-
-            id++;
-        }
-        input.value="";
-    }
-    
-});
-
-//dodanie obsługi eventa dla ikony plusa
-
-btnPlus.addEventListener("click", function(){
-        const toDo=input.value.trim();// dodaje trim() dla pewności że nie zostaną przekazane same spacje
-        //sprawdzamy czy z inputa nie jest przekazywana pusty string
-        if(toDo!==""){
-            addToDo(toDo, id, false, false);
-            objList.push({
-                name: toDo,
-                id: id,
-                done: false,
-                trash: false,
-            });
-
-            //dodawanie elementów do schowka (kod dodawany w każdym miejscu gdzie dopisywane są dane do tablicy z elementami listy)
-            localStorage.setItem("TODO", JSON.stringify(objList));
-
-            id++;
-        }
-        input.value="";
-    
-});
-
-//funkcja completeToDo->zmiana klas po zaznaczeniu "tick'a" przy pozycji na liście
-
-function completeToDo(element){
-    element.classList.toggle(check);
-    element.classList.toggle(uncheck);
-    element.parentNode.querySelector(".text").classList.toggle(lineThrough);
-
-    if(objList[element.id].done==true){
-        return objList[element.id].done==false;
-    }else{
-        return objList[element.id].done==true;
-    }
-}
-
-//funkcja removeToDo-> usunięcie elemntu z listy użytkownika
-
-function removeToDo(element){
-    element.parentNode.parentNode.removeChild(element.parentNode);
-    objList[element.id].trash=true;
-}
-
-//dodanie addEventListener'a do dynamicznie tworzonych elementów listy
-
-list.addEventListener("click", function(event){
-    const element=event.target; //zwraca kliknięty element wewnatrz listy
-    const elementJob=element.attributes.job.value;//pobranie atrybutu job elementu: complete || delete
-
-    if(elementJob =="complete"){
-        completeToDo(element)
-    }else if(elementJob=="delete"){
-        removeToDo(element);
-    }
-
-    //dodawanie elementów do schowka (kod dodawany w każdym miejscu gdzie dopisywane są dane do tablicy z elementami listy)
+    //current value of 'objList' variable is added to localStorage
     localStorage.setItem("TODO", JSON.stringify(objList));
+  }
+  input.value = "";
+}
 
+//entered input will be added to the list after hitting 'Enter' button
+document.addEventListener("keyup", function (event) {
+  if (event.key === "Enter") {
+    validate();
+  }
 });
 
+//addEventListener for the 'plus' icon
+btnPlus.addEventListener("click", function () {
+  validate();
+});
 
+//function completeToDo-> toggling classes for list elements(done/ not done)
+function completeToDo(element, parentElemId) {
+  element.classList.toggle(check);
+  element.classList.toggle(uncheck);
+  element.parentNode.querySelector(".text").classList.toggle(lineThrough);
+  objList
+    .filter((item) => item.id === parentElemId)
+    .map((item) => (item.done = !item.done));
+}
 
+//function removeToDo-> for removing list element after clicking on 'trash' icon + updating data in objList
+function removeToDo(parentElemId) {
+  const listEl = document.getElementById(parentElemId);
+  objList.map((item) =>
+    item.id === parentElemId ? (item.trash = true) : null
+  );
+  list.removeChild(listEl);
+}
+
+list.addEventListener("click", function (event) {
+  const element = event.target; 
+  const elementJob = element.attributes.job.value; //getting the value of element's 'job' attribute: complete || delete
+  const parentElemId = element.parentNode.getAttribute("id");
+
+  if (elementJob === "complete") {
+    completeToDo(element, parentElemId);
+  } else if (elementJob === "delete") {
+    removeToDo(parentElemId);
+  }
+
+  //updating data stored in the localStorage
+  localStorage.setItem("TODO", JSON.stringify(objList));
+});
