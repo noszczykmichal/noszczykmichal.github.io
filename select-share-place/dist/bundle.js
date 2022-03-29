b3b1d28e6551d37b214b43eae18a63898f5a0ccf
Michał Noszczyk <noszczykmichal@wp.pl>
2022-03-29 07:05:11 +0200

    Update bundle.js

--- select-share-place/dist/bundle.js
+++ select-share-place/dist/bundle.js
@@ -2490,7 +2490,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const axios_1 = __importDefault(__webpack_require__(/*! axios */ "./node_modules/axios/index.js"));
 const form = document.querySelector("form");
 const addressInput = document.getElementById("address");
-const google_api_key = "AIzaSyBUQ41BeKHEJwTcQYCuo7JeV1qlQu1pUwY";
+const google_api_key = "AIzaSyDn0fzPJJQ-BPIVKBUM-ka0z5ilsAMOews";
 function searchAddressHandler(event) {
     event.preventDefault();
     const enteredAddress = addressInput.value;
